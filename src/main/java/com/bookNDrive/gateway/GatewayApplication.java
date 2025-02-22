a06dee74304b931d61f1a6487c30e139951493ce
razzak <khalfallah.razzak@gmail.com>
2025-02-22 01:41:47 +0100

    add config

--- src/main/java/com/bookNDrive/gateway/GatewayApplication.java
+++ src/main/java/com/bookNDrive/gateway/GatewayApplication.java
@@ -2,12 +2,26 @@ package com.bookNDrive.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route(path -> path
+						.path("/user-service/**")
+						.filters(f -> f.rewritePath("/user-service/(?<segment>.*)","/${segment}"))
+						.uri("lb://USER-SERVICE")
+				)
+				.build();
 	}
 
 }
